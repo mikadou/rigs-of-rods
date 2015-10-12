14e4b652a557242dd722e35592bbbdfd6e8d7f51
mikadou <mikadou@users.noreply.github.com>
2015-10-12 15:07:01 +0200

    Automate building of documentation (WIP).

--- tools/travis/linux/dependencies.sh
+++ tools/travis/linux/dependencies.sh
@@ -8,7 +8,7 @@ libgl1-mesa-dev libglu1-mesa-dev libopenal-dev  \
 libx11-dev libxt-dev libxaw7-dev libxrandr-dev \
 libssl-dev libcurl4-openssl-dev libgtk2.0-dev libwxgtk3.0-dev
 
-sudo apt-get install doxygen=1.8.7~precise1 graphviz
+sudo apt-get install doxygen=1.8.7-2~precise1 graphviz
 
 doxygen --version
 exit
