6575fdecd8299f672dcb73b0054b5e62fa10fe26
mikadou <mikadou@users.noreply.github.com>
2015-10-12 11:23:37 +0200

    Automate building of documentation (WIP).

--- tools/travis/linux/build.sh
+++ tools/travis/linux/build.sh
@@ -9,3 +9,17 @@ cmake -DROR_USE_MYGUI="TRUE" \
 .
 
 make doc-doxygen 
+
+
+cd doc/doxygen/html
+git init
+git config user.name "${GH_USERNAME}"
+git config user.email "${GH_EMAILADDRESS}"
+
+git remote add upstream "https://${GH_TOKEN}@github.com/${GH_USERNAME}/rigs-of-rods.git"
+git fetch upstream
+git reset upstream/gh-pages
+
+git add -A
+git commit -m "[AUTO] Update documentation."
+git push upstream gh-pages
