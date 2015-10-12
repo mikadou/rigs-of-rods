878b15b6727ad48f22ac6a2754662279e9d1dae7
mikadou <mikadou@users.noreply.github.com>
2015-10-12 18:47:46 +0200

    Reenable building executable.

--- tools/travis/linux/build.sh
+++ tools/travis/linux/build.sh
@@ -1,3 +1,6 @@
+mkdir -p ./build
+cd ./build
+
 cmake -DROR_USE_MYGUI="TRUE" \
 -DROR_USE_OPENAL="TRUE" \
 -DROR_USE_SOCKETW="TRUE" \
@@ -6,3 +9,6 @@ cmake -DROR_USE_MYGUI="TRUE" \
 -DROR_USE_ANGELSCRIPT="TRUE" \
 -DCMAKE_CXX_FLAGS="-O0" \
 -DBUILD_DOC_DOXYGEN="TRUE"
+..
+
+cd ..
